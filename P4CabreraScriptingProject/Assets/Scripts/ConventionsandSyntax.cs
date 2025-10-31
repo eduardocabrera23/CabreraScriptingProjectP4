8d6f22217228e540322eaf63e671bb31bf8d0fb5
eduardocabrera23 <768550@student.dusd.net>
2025-10-31 10:48:42 -0700

    did new scripting project
    Added conventionss and syntax

---
+++ P4CabreraScriptingProject/Assets/Scripts/ConventionsandSyntax.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsandSyntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this line is there to tell me the x position of my object
+        Debug.Log(transform.position.x);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= 5F)
+        {
+            Debug.Log(" I'm about to hit the ground!");
+        }
+    }
+}
